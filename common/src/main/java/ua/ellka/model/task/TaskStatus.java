0895142fe216ea14e80ef9927e6f304f1ef52cc9
vkalkatins <vkalkatins@gmail.com>
2024-12-22 18:56:32 +0200

    6: [FEATURE] Modificate enums in model package

--- common/src/main/java/ua/ellka/model/task/TaskStatus.java
+++ common/src/main/java/ua/ellka/model/task/TaskStatus.java
@@ -1,9 +1,26 @@
 package ua.ellka.model.task;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum TaskStatus {
-    TODO,
-    IN_PROGRESS,
-    NEEDS_REVIEW,
-    BLOCKED,
-    COMPLETED
+    NOT_STARTED ("Not Started"),
+    TODO ("TODO"),
+    IN_PROGRESS ("In Progress"),
+    NEEDS_REVIEW ("Needs Review"),
+    BLOCKED ("Blocked"),
+    COMPLETED ("Completed"),;
+
+    private final String status;
+
+    public static TaskStatus fromStatus(String status) {
+        for (TaskStatus taskStatus : TaskStatus.values()) {
+            if (taskStatus.getStatus().equalsIgnoreCase(status)) {
+                return taskStatus;
+            }
+        }
+        return TaskStatus.NOT_STARTED;
+    }
 }
