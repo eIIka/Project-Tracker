dacc7421206a693a8bda1103ed7a3db68babd6ef
vkalkatins <vkalkatins@gmail.com>
2025-01-27 19:12:35 +0200

    [BUG] Fix and optimize mappers for model classes

--- project-tracker-common/src/main/java/ua/ellka/mapper/TaskMapper.java
+++ project-tracker-common/src/main/java/ua/ellka/mapper/TaskMapper.java
@@ -1,15 +1,65 @@
 package ua.ellka.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 import ua.ellka.dto.TaskDTO;
+import ua.ellka.model.project.Project;
 import ua.ellka.model.task.Task;
+import ua.ellka.model.task.TaskStatus;
+import ua.ellka.model.user.Employee;
+import ua.ellka.model.user.Manager;
 
 @Mapper (unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface TaskMapper {
     TaskMapper INSTANCE = Mappers.getMapper(TaskMapper.class);
 
+    @Mapping(source = "status.status", target = "status")
+    @Mapping(source = "manager.nickname", target = "assignedManager")
+    @Mapping(source = "employee.nickname", target = "assignedEmployee")
+    @Mapping(source = "project.name", target = "projectName")
     TaskDTO taskToTaskDTO(Task task);
+
+    @Mapping(source = "status", target = "status", qualifiedByName = "mapStatus")
+    @Mapping(source = "assignedManager", target = "manager", qualifiedByName = "mapManager")
+    @Mapping(source = "assignedEmployee", target = "employee", qualifiedByName = "mapEmployee")
+    @Mapping(source = "projectName", target = "project", qualifiedByName = "mapProject")
     Task taskDTOToTask(TaskDTO taskDTO);
+
+    @Named("mapStatus")
+    default TaskStatus mapStatus(String status) {
+        return TaskStatus.fromString(status);
+    }
+
+    @Named("mapManager")
+    default Manager mapManager(String managerName) {
+        if (managerName == null) {
+            return null;
+        }
+        Manager manager = new Manager();
+        manager.setNickname(managerName);
+        return manager;
+    }
+
+    @Named("mapEmployee")
+    default Employee mapEmployee(String employeeName) {
+        if (employeeName == null) {
+            return null;
+        }
+        Employee employee = new Employee();
+        employee.setNickname(employeeName);
+        return employee;
+    }
+
+    @Named("mapProject")
+    default Project mapProject(String projectName) {
+        if (projectName == null) {
+            return null;
+        }
+        Project project = new Project();
+        project.setName(projectName);
+        return project;
+    }
 }
--- project-tracker-common/src/main/java/ua/ellka/mapper/UserMapper.java
+++ project-tracker-common/src/main/java/ua/ellka/mapper/UserMapper.java
@@ -1,20 +1,36 @@
 package ua.ellka.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 import ua.ellka.dto.EmployeeDTO;
 import ua.ellka.dto.ManagerDTO;
 import ua.ellka.model.user.Employee;
 import ua.ellka.model.user.Manager;
+import ua.ellka.model.user.UserRole;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
-    EmployeeDTO toEmployeeDTO(Employee employee);
-    Employee toEmployee(EmployeeDTO employeeDTO);
+    @Mapping(target = "taskCount", expression = "java(employee.getTasks() != null ? (long) employee.getTasks().size() : 0L)")
+    @Mapping(target = "projectCount", expression = "java(employee.getProjects() != null ? (long) employee.getProjects().size() : 0L)")
+    EmployeeDTO employeeToEmployeeDTO(Employee employee);
 
-    ManagerDTO toManagerDTO(Manager manager);
-    Manager toManager(ManagerDTO managerDTO);
+    @Mapping(source = "role", target = "role", qualifiedByName = "mapStatus")
+    Employee employeeDTOToEmployee(EmployeeDTO employeeDTO);
+
+    @Mapping(target = "projectCount", expression = "java(manager.getProjects() != null ? (long) manager.getProjects().size() : 0L)")
+    ManagerDTO managerToManagerDTO(Manager manager);
+
+    @Mapping(source = "role", target = "role", qualifiedByName = "mapStatus")
+    Manager managerDTOToManager(ManagerDTO managerDTO);
+
+    @Named("mapStatus")
+    default UserRole mapStatus(String role) {
+        return UserRole.fromString(role);
+    }
+
 }
