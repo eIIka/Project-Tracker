9ae032a856ff54070ac085a4c8a58cdf3c485220
vkalkatins <vkalkatins@gmail.com>
2025-02-20 23:58:29 +0200

    [FEATURE] Add prototype scope to jdbcTemplate bean

--- project-tracker-dao/src/test/java/ua/ellka/config/DaoTestConfig.java
+++ project-tracker-dao/src/test/java/ua/ellka/config/DaoTestConfig.java
@@ -2,14 +2,18 @@ package ua.ellka.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
+
 @Configuration
 public class DaoTestConfig {
     @Bean
+    @Scope(SCOPE_PROTOTYPE)
     public JdbcTemplate jdbcTemplate() {
         EmbeddedDatabase dataSource = new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
